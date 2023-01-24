b4f1da2a7a7e25913507a5c19d712b0ce0729c45
AlexSoloviov94 <sanyas9435@gmail.com>
2023-01-24 19:54:55 +0200

    добавление файлов с помощью jsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const h1 = React.createElement('h1', { id: 'title' }, 'Hello React')
+// jsx
+const h1 = <h1 id="title">Hello React.js</h1>
 console.log(h1)
 
-const list = React.createElement(
-    'ul',
-    null,
-    React.createElement('li', null, 'list Item 1'),
-    React.createElement('li', null, 'list Item 2'),
-    React.createElement('li', null, 'list Item 3')
+const list = (
+    <ul>
+        <li>List Item 1</li>
+        <li>List Item 2</li>
+        <li>List Item 3</li>
+    </ul>
 )
 
 const content = (
