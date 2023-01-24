b9247388991f5928f1000ff675a51e7f7cc42ef2
AlexSoloviov94 <sanyas9435@gmail.com>
2023-01-24 19:44:15 +0200

    создание контента на странице

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+const h1 = React.createElement('h1', { id: 'title' }, 'Hello React')
+console.log(h1)
+
+const list = React.createElement(
+    'ul',
+    null,
+    React.createElement('li', null, 'list Item 1'),
+    React.createElement('li', null, 'list Item 2'),
+    React.createElement('li', null, 'list Item 3')
+)
+
+const content = (
+    <div>
+        {h1}
+        {list}
+    </div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(<React.StrictMode>{content}</React.StrictMode>)
