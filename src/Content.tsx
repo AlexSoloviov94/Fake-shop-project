0920dc5ac94a66c2fc38e89c7bed271d47316ff5
AlexSoloviov94 <sanyas9435@gmail.com>
2023-01-26 20:15:04 +0200

    добавили файл css

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,3 +1,5 @@
+import './Content.css'
+
 type Props = {
     text1: string
     text2: string
@@ -8,15 +10,7 @@ export const Content = (props: Props) => {
     let data = true
     return (
         <>
-            <p
-                style={{
-                    color: data ? 'yellow' : 'white',
-                    backgroundColor: 'green',
-                    padding: '15px',
-                }}
-            >
-                {props.text1}
-            </p>
+            <p className="content">{props.text1}</p>
             <p>{props.text2}</p>
             <div>Year {props.year}</div>
         </>
