6fac945137ca45aa67c38ce9e161db5b27905c5d
AlexSoloviov94 <sanyas9435@gmail.com>
2023-01-26 20:38:50 +0200

    добавили baseUrl в файл tsconfig

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Content } from './Content'
-import Title from './Title'
-
-function App() {
-    return (
-        <>
-            <Title text="React" />
-            <Title text="TS" />
-            <Content text1="Go Go" text2="hello world 2" year={2023} />
-        </>
-    )
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+root.render(<React.StrictMode></React.StrictMode>)
