f6791aa4b970b1a75fddc11d914c17e7554c14df
AlexSoloviov94 <sanyas9435@gmail.com>
2023-01-28 19:14:38 +0200

    создали папку контейнер подготавливаем к верстке проекта

---
+++ src/container/App/App.tsx
@@ -0,0 +1,15 @@
+import Footer from 'container/Footer/Footer'
+import Header from 'container/Header/Header'
+import Main from 'container/Main/Main'
+
+type Props = {}
+const App = (props: Props) => {
+    return (
+        <>
+            <Header />
+            <Main />
+            <Footer />
+        </>
+    )
+}
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
+import App from 'container/App/App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
