e2cfa1cf0e5d7207ea16dc68e4c3534f7e673938
AlexSoloviov94 <sanyas9435@gmail.com>
2023-01-31 19:32:50 +0200

    добавили библиотеку и внесли шапку

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,11 +1,13 @@
 import Footer from 'container/Footer/Footer'
 import Header from 'container/Header/Header'
 import Main from 'container/Main/Main'
+import CssBaseline from '@mui/material/CssBaseline'
 
 type Props = {}
 const App = (props: Props) => {
     return (
         <>
+            <CssBaseline />
             <Header />
             <Main />
             <Footer />
