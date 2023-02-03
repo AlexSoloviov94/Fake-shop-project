689a6623a5a05f2a779395d1b3e60ed521099730
AlexSoloviov94 <sanyas9435@gmail.com>
2023-02-03 22:52:16 +0200

    добавили productsArray

--- src/components/ProductsList/ProductsList.tsx
+++ src/components/ProductsList/ProductsList.tsx
@@ -1,7 +1,16 @@
 import { Grid, Typography } from '@mui/material'
 import ProductsListItem from './ProductsListItem'
+import productsArray from 'utils/productsArray'
 
 type Props = {}
+
+type ProductProps = {
+    title: string
+    description: string
+    type: string
+    capacity: string
+    price: number
+}
 
 const ProductsList = (props: Props) => {
     return (
@@ -11,36 +20,26 @@ const ProductsList = (props: Props) => {
             </Typography>
 
             <Grid container spacing={3}>
-                <Grid item xs={12} sm={6} md={4}>
-                    {''}
-                    <ProductsListItem
-                        title="IPhone 14 Pro"
-                        description="This IPhone 14 Pro"
-                        type="phone"
-                        capacity="256 "
-                        price={1200}
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    {''}
-                    <ProductsListItem
-                        title="IPhone 12 Pro"
-                        description="This IPhone 12 Pro"
-                        type="phone"
-                        capacity="128 "
-                        price={900}
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6} md={4}>
-                    {''}
-                    <ProductsListItem
-                        title="IPhone 13"
-                        description="This IPhone 13"
-                        type="phone"
-                        capacity="512 "
-                        price={1000}
-                    />
-                </Grid>
+                {productsArray.map(
+                    ({
+                        title,
+                        description,
+                        type,
+                        capacity,
+                        price,
+                    }: ProductProps) => (
+                        <Grid item xs={12} sm={6} md={4}>
+                            {''}
+                            <ProductsListItem
+                                title={title}
+                                description={description}
+                                type={type}
+                                capacity={capacity}
+                                price={price}
+                            />
+                        </Grid>
+                    )
+                )}
             </Grid>
         </>
     )
---
+++ src/utils/productsArray.ts
@@ -0,0 +1,68 @@
+type ProductProps = {
+    title: string
+    description: string
+    type: string
+    capacity: string
+    price: number
+}
+
+const productsArray: ProductProps [] = [
+    {
+        title: 'IPhone 14 Pro',
+        description: 'This is IPhone 14 Pro',
+        type: 'phone',
+        capacity: '256',
+        price: 1200,
+    },
+    {
+        title: 'IPhone 13 Pro',
+        description: 'This is IPhone 13 Pro',
+        type: 'phone',
+        capacity: '256',
+        price: 1000,
+    },
+    {
+        title: 'IPhone 12 Pro',
+        description: 'This is IPhone 12 Pro',
+        type: 'phone',
+        capacity: '128',
+        price: 900,
+    },
+    {
+        title: 'IPhone 11 Pro',
+        description: 'This is IPhone 11 Pro',
+        type: 'phone',
+        capacity: '512',
+        price: 850,
+    },
+    {
+        title: 'IPhone 11 ',
+        description: 'This is IPhone 11',
+        type: 'phone',
+        capacity: '256',
+        price: 800,
+    },
+    {
+        title: 'IPhone 10 Pro',
+        description: 'This is IPhone 10 Pro',
+        type: 'phone',
+        capacity: '256',
+        price: 800,
+    },
+    {
+        title: 'IPhone X ',
+        description: 'This is IPhone X',
+        type: 'phone',
+        capacity: '256',
+        price: 750,
+    },
+    {
+        title: 'IPhone 9S',
+        description: 'This is IPhone 9S',
+        type: 'phone',
+        capacity: '128',
+        price: 600,
+    },
+]
+
+export default productsArray
