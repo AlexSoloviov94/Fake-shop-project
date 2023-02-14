855717eb8835249e392fa1bcbdccc82cb3cb52c7
AlexSoloviov94 <sanyas9435@gmail.com>
2023-02-14 20:54:00 +0200

    сделали функцию для добавления в корзину

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -14,14 +14,24 @@ type CartDataProps = {
 
 const App = (props: Props) => {
     const [cartData, setCartData] = useState<CartDataProps>({
-        totalCount: 10,
-        totalPrice: 100,
+        totalCount: 0,
+        totalPrice: 0,
     })
+
+    const addProductToCart = (count: number, price: number) => {
+        setCartData((prevState) => ({
+            totalCount: prevState.totalCount + count,
+            totalPrice: prevState.totalPrice + count * price,
+        }))
+    }
 
     return (
         <StyledEngineProvider injectFirst>
             <CssBaseline />
             <Header cartData={cartData} />
+            <button onClick={() => addProductToCart(5, 500)}>
+                Add to cart
+            </button>
             <Main />
             <Footer />
         </StyledEngineProvider>
